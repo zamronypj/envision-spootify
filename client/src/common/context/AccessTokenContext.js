2ab354fa0e69746c5a6ee8ea0ee97c0033d70172
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-12-26 11:49:17 +0800

    add comment to explain code purpose

--- client/src/common/context/AccessTokenContext.js
+++ client/src/common/context/AccessTokenContext.js
@@ -1,3 +1,7 @@
+/**
+ * Context data for access token that will be passed down
+ * to inner children so that we can avoid props drilling
+ */
 import React, { createContext } from "react";
 
 export const AccessTokenContext = createContext();
